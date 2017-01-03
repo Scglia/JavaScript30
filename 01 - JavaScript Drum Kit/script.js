3500963ac20e61ba680e1eccc2c6b0f37fe7c8a5
Scglia <Scglia@users.noreply.github.com>
2017-01-03 15:59:36 +0100

    removing event listeners on detach

--- 01 - JavaScript Drum Kit/script.js
+++ 01 - JavaScript Drum Kit/script.js
@@ -8,17 +8,24 @@ class SoundKey extends HTMLElement {
     this.classList.add('playing');
   }
 
+  onTransitionend (e) {
+    this.classList.remove('playing');
+  }
+
   createdCallback () {
     this._key = this.getElementsByTagName('kbd')[0].innerHTML.toLowerCase();
     this._audio = this.getElementsByTagName('audio')[0];
+    this.onKeydown = this.onKeydown.bind(this);
   }
 
   attachedCallback () {
-    window.addEventListener('keydown', this.onKeydown.bind(this));
-    this.addEventListener('transitionend', () => this.classList.remove('playing'));
+    window.addEventListener('keydown', this.onKeydown);
+    this.addEventListener('transitionend', this.onTransitionend);
   }
 
   detachedCallback () {
+    window.removeEventListener('keydown', this.onKeydown);
+    this.removeEventListener('transitionend', this.onTransitionend);
   }
 }
 
